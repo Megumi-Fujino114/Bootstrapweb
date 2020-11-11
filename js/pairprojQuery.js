a13a4e7c3e32337584f89bcfdbc4f83d66bdc576
Megumi fujino <>
2020-11-11 16:51:32 +0900

    jsファイルの追加　一部文字の変更

---
+++ js/pairprojQuery.js
@@ -0,0 +1,27 @@
+let d1_jump = $("#d1-jump").offset().top;
+$("#d1").click(function(){
+    $("html,body").animate({scrollTop:d1_jump});
+})
+
+let d2_jump = $("#d2-jump").offset().top;
+$("#d2").click(function(){
+    $("html,body").animate({scrollTop:d2_jump});
+})
+
+let d3_jump = $("#d3-jump").offset().top;
+$("#d3").click(function(){
+    $("html,body").animate({scrollTop:d3_jump});
+})
+
+let d4_jump = $("#d4-jump").offset().top;
+$("#d4").click(function(){
+    $("html,body").animate({scrollTop:d4_jump});
+})
+let d5_jump = $("#d5-jump").offset().top;
+$("#d5").click(function(){
+    $("html,body").animate({scrollTop:d5_jump});
+})
+let d6_jump = $("#d6-jump").offset().top;
+$("#d6").click(function(){
+    $("html,body").animate({scrollTop:d6_jump});
+})
