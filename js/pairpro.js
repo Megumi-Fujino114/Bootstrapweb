363e5bcd8d315a10036f628daccb4337f39142f5
Megumi fujino <>
2020-11-11 16:15:12 +0900

    スライド追加

--- js/pairpro.js
+++ js/pairpro.js
@@ -6,4 +6,9 @@ var mySwiper = new Swiper ('.swiper-container', {
             pagination: '.swiper-pagination',
             nextButton: '.swiper-button-next',
             prevButton: '.swiper-button-prev',
+            speed: 3000,
+            autoplay: {
+            delay: 8000,
+            disableOnInteraction: true
+  },
         });
